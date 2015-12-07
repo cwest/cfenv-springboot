e291accd84617eefb6ee6cdee2102f10fd472297
Casey West <casey@geeknest.com>
2015-12-07 08:24:41 -0500

    Adding analytics backed by redis.

---
+++ src/main/java/cfenv/Analytics.java
@@ -0,0 +1,20 @@
+package cfenv;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.support.atomic.RedisAtomicLong;
+
+public class Analytics {
+	private final StringRedisTemplate template;
+
+	private final RedisAtomicLong visits;
+
+	public Analytics(StringRedisTemplate template) {
+		this.template = template;
+
+		visits = new RedisAtomicLong("visits", template.getConnectionFactory());
+	}
+
+	public String getVisits() {
+		return String.valueOf(visits.incrementAndGet());
+	}
+}
--- src/main/java/cfenv/CFEnvController.java
+++ src/main/java/cfenv/CFEnvController.java
@@ -3,16 +3,29 @@ package cfenv;
 import java.util.Map;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class CFEnvController {
+	@Autowired
+	private final Analytics analytics;
+
+	@Autowired
+	public CFEnvController(Analytics analytics) {
+		this.analytics = analytics;
+	}
+
 	@RequestMapping("/")
 	public CFEnv cfenv() {
-		return new CFEnv(getInstanceFromEnv());
+		return new CFEnv(getInstanceFromEnv(), getVisits());
 	}
 
 	private String getInstanceFromEnv() {
 		Map<String, String> env = System.getenv();
 		return env.get("CF_INSTANCE_INDEX");
+	}
+
+	private String getVisits() {
+		return analytics.getVisits();
 	}
 }
